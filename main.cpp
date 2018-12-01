4fceb28b7c03a1145a51238a09ad71aba2806240
Armando Herrera <anhydrous99@gmail.com>
2018-12-01 13:37:34 -0600

    Make sure the sorts were successful

--- main.cpp
+++ main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
   auto t3 = hrc::now();
   bubblesort(bsort_vec.begin(), bsort_vec.end());
   auto t4 = hrc::now();
+  if (!(bsort_vec == default_vec)) std::cerr << "Bubble Sort Failed.\n";
 
   // use Gnome Sort
   d_vector gsort_vec = random_vector;
@@ -50,6 +51,7 @@ int main(int argc, char *argv[]) {
   auto t5 = hrc::now();
   gnomesort(gsort_vec.begin(), gsort_vec.end());
   auto t6 = hrc::now();
+  if (!(gsort_vec == default_vec)) std::cerr << "Gnome Sort Failed.\n";
 
   // use Heap Sort
   d_vector heap_vec = random_vector;
@@ -57,6 +59,7 @@ int main(int argc, char *argv[]) {
   auto t7 = hrc::now();
   heapsort(heap_vec.begin(), heap_vec.end());
   auto t8 = hrc::now();
+  if (!(heap_vec == default_vec)) std::cerr << "Heap Sort Failed.\n";
 
   // use Insertion Sort
   d_vector ins_vec = random_vector;
@@ -64,6 +67,7 @@ int main(int argc, char *argv[]) {
   auto t9 = hrc::now();
   insertionsort(ins_vec.begin(), ins_vec.end());
   auto t10 = hrc::now();
+  if (!(ins_vec == default_vec)) std::cerr << "Insertion Sort Failed.\n";
 
   // use Shell Sort
   d_vector shell_vec = random_vector;
@@ -71,6 +75,7 @@ int main(int argc, char *argv[]) {
   auto t11 = hrc::now();
   shellsort(shell_vec.begin(), shell_vec.end());
   auto t12 = hrc::now();
+  if (!(shell_vec == default_vec)) std::cerr << "Shell Sort Failed.\n";
 
   // use Merge Sort
   d_vector merge_vec = random_vector;
@@ -78,6 +83,7 @@ int main(int argc, char *argv[]) {
   auto t13 = hrc::now();
   mergesort(merge_vec.begin(), merge_vec.end());
   auto t14 = hrc::now();
+  if (!(merge_vec == default_vec)) std::cerr << "Mergesort Failed.\n";
 
   // use Selection Sort
   d_vector selection_vec = random_vector;
@@ -85,6 +91,7 @@ int main(int argc, char *argv[]) {
   auto t15 = hrc::now();
   selectionsort(selection_vec.begin(), selection_vec.end());
   auto t16 = hrc::now();
+  if (!(selection_vec == default_vec)) std::cerr << "Selection Sort Failed.\n";
 
   // use Odd-even Sort
   d_vector oddeven_vec = random_vector;
@@ -92,6 +99,7 @@ int main(int argc, char *argv[]) {
   auto t17 = hrc::now();
   oddevensort(oddeven_vec.begin(), oddeven_vec.end());
   auto t18 = hrc::now();
+  if (!(oddeven_vec == default_vec)) std::cerr << "Odd-Even Sort Failed.\n";
 
   // use Comb Sort
   d_vector comb_vec = random_vector;
@@ -99,6 +107,7 @@ int main(int argc, char *argv[]) {
   auto t19 = hrc::now();
   combsort(comb_vec.begin(), comb_vec.end());
   auto t20 = hrc::now();
+  if (!(comb_vec == default_vec)) std::cerr << "Comb Sort Failed.\n";
 
   // Output results
   std::cout << "std::sort: " << duration_cast<microseconds>(t2 - t1).count() << " microseconds\n";
